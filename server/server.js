c5aa75ba3b900428cfb4819e2415bb40dab2a19a
mailis <mailis.kokla@gmail.com>
2018-01-05 13:48:08 +0200

    Sdd POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -100,4 +100,17 @@ app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 module.exports = {app};
